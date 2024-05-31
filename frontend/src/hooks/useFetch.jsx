09ba13d877466b62f800822aad37ca0b791870b9
Arman-Kabir <armankabir8@gmail.com>
2024-06-01 00:46:29 +0600

    useEffect and home reviews shoed

---
+++ frontend/src/hooks/useFetch.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+
+const useFetch = (url) => {
+  const [data, setData] = useState();
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  console.log(url)
+
+  console.log(data);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const res = await fetch(url);
+        if (!res.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const json = await res.json();
+
+        setData(json.data);
+        setLoading(false);
+      } catch (error) {
+        setError(error);
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, [url]);
+
+  return { loading, error, data };
+};
+
+export default useFetch;
--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,8 +1,30 @@
+import useFetch from "../hooks/useFetch";
+import { Link } from "react-router-dom";
 
 export default function Homepage() {
+  const { loading, error, data } = useFetch(
+    "http://localhost:8082/api/reviews"
+  );
+  if (loading) {
+    return <p>Loading .....</p>;
+  }
+  if (error) {
+    return <p>Error .....</p>;
+  }
   return (
     <div>
-        fgdf
+      { Array.isArray(data) && data.map((review) => (
+        <div key={review.id} className="review-card">
+          {
+            console.log(review)
+          }
+          <div className="rating">{review.attributes.rating}</div>
+          <h2>{review.attributes.title}</h2>
+          <small>console list</small>
+          <p>{review.attributes.body}</p>
+          <Link to={`/details/${review.attributes.id}`}>Read More</Link>
+        </div>
+      ))}
     </div>
-  )
+  );
 }
